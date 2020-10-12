4bea3535282296d0a53a063fdd912f829717b6cc
Oscar Sjöcrona <oscarsjocrona@gmail.com>
2020-10-12 14:12:28 +0200

    Removed build from gitnore. Added build folder

---
+++ build/precache-manifest.e10126960838cb974325c2bc491ffa74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3baaae91919feb707ee14b6fd0852dfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd260754552b6347e95e",
+    "url": "/static/css/main.c4a9dfd9.chunk.css"
+  },
+  {
+    "revision": "d3eb08f9a5ee1533724a",
+    "url": "/static/js/2.e0155f6c.chunk.js"
+  },
+  {
+    "revision": "9f90cde4396c52b73cc0a6bcd8267b7b",
+    "url": "/static/js/2.e0155f6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd260754552b6347e95e",
+    "url": "/static/js/main.ac22ab15.chunk.js"
+  },
+  {
+    "revision": "caf43f0a14314da69f12",
+    "url": "/static/js/runtime-main.226f7c93.js"
+  }
+]);
